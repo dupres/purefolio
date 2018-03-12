c2eeab18c2a49cf56fd039ca1ccc36c625445b66
dupres <stephen.dupre@hotmail.fr>
2018-03-12 16:16:32 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>DUPRE Stephen - Portfolio</title>
+  </head>
+  <body>
+
+    <div class="container">
+    	<div class="row">
+    		<div class="col-" id="p-menu">
+    		</div>
+    		<div class="col">
+		    	<div id="me" class="container part">
+			    	<div class="row" id="hi">
+			    		<img src="hi.jpg"/>
+			    	</div>
+			    	<div class="row hitxt">My name is Stephen DUPRE, and this is my portfolio !</div>
+			    	<div class="row hitxt">I am a 3rd year IT student, currently learning in Licence pro DIM in Annecy (France).</div>
+			    	<div class="row hitxt">I learn JS tricks and mobile stuff, in order to become a competent and agile web developper.</div>
+			    	<div class="row hitxt">I like to test and experiment new things.</div>
+			    	<div class="row hitxt">What I enjoy the most is to put my knowledge into practice.</div>
+			    	<div class="row hitxt">Have a check on my apps list at the bottom of this page !</div>
+			    </div>
+
+			    <div id="skills" class="container part">
+			    	skills
+			    </div>
+
+			    <div id="experiences" class="container part">
+			    	experiences
+			    </div>
+
+			    <div id="projects" class="container part">
+			    	projects
+			    </div>
+			</div>
+		</div>
+	</div>
+
+
+    <style>
+    	@font-face {
+		    font-family: roboto;
+		    src: url(./Roboto-Regular.ttf);
+		}
+		body{
+			font-family:roboto;
+			padding-top:30px;
+			background-image: url(bg.jpg);
+			background-size: cover;
+		}
+		h1{
+
+		}
+
+		.part{
+			background-color: lightgray;
+			border-radius:3vh;
+			padding:30px;
+			height:90vh;
+			margin-bottom:20vh;
+		}
+		#hi{
+			display:flex;
+			flex-direction: column;
+			align-items:center;
+			margin-bottom:5vh;
+		}
+		.hitxt{
+			font-size:1.5em;
+		}
+
+	</style>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
